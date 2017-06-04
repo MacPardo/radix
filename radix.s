f2ee64982f1df3daa10fae712e3da40707e070a7
Marcos Adamczuk <marcos.adamczuk@gmail.com>
2017-06-04 03:02:37 -0300

    terminei menu

--- radix.s
+++ radix.s
@@ -1,8 +1,29 @@
         .data
-txt_menu:       "\nMenu"
+txt_menu:       "\nMenu\n1 Inserir\n2 Remover\n3 Buscar\n4 Ordenar\n5 Exibir\n6 Sair\nOpcao: "
         .text
-        la $a0, txt_menu
-        li $v0, 4
+main:
+        li   $s0, 0     #s0 aponta para o inicio da lista, inicialmente null
+mostra_menu:    
+        la   $a0, txt_menu
+        li   $v0, 4
         syscall
-        li $v0, 5
+        li   $v0, 5
         syscall
+        li   $t0, 1
+        beq  $v0, $t0, inserir
+        li   $t0, 2
+        beq  $v0, $t0, remover
+        li   $t0, 3
+        beq  $v0, $t0, buscar
+        li   $t0, 4
+        beq  $v0, $t0, ordenar
+        li   $t0, 5
+        beq  $v0, $t0, exibir
+
+        #se nao foi nenhuma dessas opcoes, sai do programa
+        li   $v0, 10
+        syscall
+
+inserir:        
+
+
