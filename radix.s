bd32415aff2e5931a76f7fe730d8bafc027dfb38
Marcos Adamczuk <marcos.adamczuk@gmail.com>
2017-06-04 15:55:32 -0300

    terminei parte de inserir sem testar

--- radix.s
+++ radix.s
@@ -3,8 +3,8 @@ txt_menu:       "\nMenu\n1 Inserir\n2 Remover\n3 Buscar\n4 Ordenar\n5 Exibir\n6 
 txt_insere:     "\nValor a inserir: "
         .text
 main:
-        li   $s0, 0     #s0 aponta para o inicio da lista, inicialmente null
-mostra_menu:    
+        li   $s0, 0     #s0 aponta para o inicio da lista, inicialmente NULL
+menu:    
         la   $a0, txt_menu
         li   $v0, 4
         syscall
@@ -28,7 +28,44 @@ mostra_menu:
 inserir:        
         li   $v0, 5
         la   $a0, txt_insere
-        syscall
+        syscall         #print txt_insere
         li   $v0, 4
-        syscall
-        move $a0, $v0
+        syscall         #le valor a inserir
+        move $t0, $v0   #t0 eh o valor a inserir
+        beqz $s0, inserir_primeiro #se a lista eh vazia, insere o primeiro elemento
+
+        #se nao, insere sempre no final da lista
+        move $t1, $s0   #t1 eh o valor atual
+
+        #t1 tem que apontar para o elemento cujo proximo elemento eh NULL
+ins_loop_beg:   
+        lw   $t2, 8 ($t1) #t2 aponta para t1->prox
+        beqz $t2, ins_loop_end #se t1->prox == NULL, sai do loop
+
+        move $t1, $t2   #se nao, t1 = t1->prox
+        j    ins_loop_beg
+ins_loop_end:   
+        #agora, $t1 aponta para o ultimo elemento
+
+        li   $a0, 12
+        li   $v0, 9
+        syscall         #alocar memoria para o novo elemento
+
+        #v0 aponta para novo elemento
+
+        sw   $v0, 8 ($t1) #t1->prox = v0
+        sw   $t0, 0 ($v0) #valor do novo elemento eh t0
+        sw   $t1, 4 ($v0) #v0->ant = $t1
+        sw   $zero, 8 ($v0) #v0->prox = NULL
+
+        j    menu
+
+inserir_primeiro:       
+        li   $a0, 12
+        li   $v0, 9
+        syscall         #aloca 12 bytes de memoria
+        move $s0, $v0   #s0 aponta para o comeco da lista, que nao eh mais NULL
+        sw   $t0, 0 ($s0) #primeiros 4 bytes armazenam o valor
+        sw   $zero, 4 ($s0) #segundos  4 bytes apontam para o anterior
+        sw   $zero, 8 ($s0) #terceiros 4 bytes apontam para o proximo
+        j    menu
