021cd5c095aa30fd27ec0685b03ed84dd392e3ae
Marcos Adamczuk <marcos.adamczuk@gmail.com>
2017-06-04 16:38:44 -0300

    terminei a parte que printa sem testar

--- radix.s
+++ radix.s
@@ -1,6 +1,9 @@
         .data
+txt_espaco:     " "
+txt_linha:      "\n"
 txt_menu:       "\nMenu\n1 Inserir\n2 Remover\n3 Buscar\n4 Ordenar\n5 Exibir\n6 Sair\nOpcao: "
 txt_insere:     "\nValor a inserir: "
+txt_vazia:      "\nLista vazia!"
         .text
 main:
         li   $s0, 0     #s0 aponta para o inicio da lista, inicialmente NULL
@@ -19,17 +22,17 @@ menu:
         li   $t0, 4
         beq  $v0, $t0, ordenar
         li   $t0, 5
-        beq  $v0, $t0, exibir
+        beq  $v0, $t0, print
 
         #se nao foi nenhuma dessas opcoes, sai do programa
         li   $v0, 10
         syscall
 
 inserir:        
-        li   $v0, 5
+        li   $v0, 4
         la   $a0, txt_insere
         syscall         #print txt_insere
-        li   $v0, 4
+        li   $v0, 5
         syscall         #le valor a inserir
         move $t0, $v0   #t0 eh o valor a inserir
         beqz $s0, inserir_primeiro #se a lista eh vazia, insere o primeiro elemento
@@ -69,3 +72,42 @@ inserir_primeiro:
         sw   $zero, 4 ($s0) #segundos  4 bytes apontam para o anterior
         sw   $zero, 8 ($s0) #terceiros 4 bytes apontam para o proximo
         j    menu
+
+#FIM DA INSERCAO
+
+
+print:
+        la   $a0, txt_linha
+        li   $v0, 4
+        syscall         #print "\n"
+
+        beqz $s0, print_vazia #se comeco da lista eh NULL
+
+        #se nao, printa os elementos
+        move $t1, $s0   #t1 aponta para o comeco da lista
+prt_loop_beg: 
+        beqz $t1, prt_loop_end #se o elemento atual for NULL, termina de printar
+        lw   $a0, 0 ($t1)
+        li   $v0, 1
+        sycall          #print t1 (elemento atual)
+
+        la   $a0, txt_espaco
+        li   $v0, 4
+        syscall         #print "\n"
+
+        lw   $t1, 8 ($t1) #t1 = t1->next
+        j    prt_loop_beg
+prt_loop_end:   
+        la   $a0, txt_linha
+        li   $v0, 4
+        syscall         #print "\n"
+
+        j    menu
+
+print_vazia:
+        la   $a0, txt_vazia
+        li   $v0, 5
+        syscall         print txt_vazia
+        j    menu
+
+#FIM DO PRINT
